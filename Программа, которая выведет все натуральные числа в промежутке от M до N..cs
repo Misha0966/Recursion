7d6bf37f7a3c45e0d061757993a13a19a552b7d0
Misha0966 <108358743+Misha0966@users.noreply.github.com>
2022-08-23 15:46:17 +0300

    Задача №64
    Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
    
    M = 1; N = 5. -> ""1, 2, 3, 4, 5""
    
    M = 4; N = 8. -> ""4, 6, 7, 8""

---
+++ Программа, которая выведет все натуральные числа в промежутке от M до N..cs
@@ -0,0 +1,29 @@
+﻿//M = 5; N = 1. -> "5, 4, 3, 2, 1"
+//M = 8; N = 4. -> "8, 7,6, 5, 4"
+
+
+//  ввод входных параметров задачи
+Console.WriteLine("Поиск натуральных чисел в промежутке от M до N: ");
+Console.Write("Введите значение M: ");
+int numberOne=Convert.ToInt32(Console.ReadLine()); // создание первой переменной
+Console.Write("Введите значение N: ");
+int numberTwo=Convert.ToInt32(Console.ReadLine()); // создание второй переменной
+string result = NumbersInRange(numberOne, numberTwo); //создание результата для строки
+Console.WriteLine($"M = {numberOne}; N = {numberTwo}. - > {result}");
+
+string NumbersInRange(int numberOne, int numberTwo) // создание номерного диапазона для строки
+{
+    if (numberOne > numberTwo) // пока первая переменная больше второй переменной, то
+    {
+        return NumbersInRange(numberOne, numberTwo+1) + ", " + numberTwo; // возвращай номерной диапазон
+    }
+    else // иначе
+    
+    if(numberOne < numberTwo) // пока первая переменная меньше второй переменной, то
+    {
+        return NumbersInRange(numberOne+1, numberTwo) + ", " + numberOne; // возвращай номерной диапазон
+    }
+    {
+        return Convert.ToString(numberTwo); // возвращай метод,который принимает два целых числа и переводит вторую переменную в эквивалентное ему строковое представление 
+    }
+}
